3a933d09be94a3514ab210d73a6966f00ec290e2
YSQ <shaoqianyi7@163.com>
2025-03-24 21:12:17 +0800

    用户注册登录模块_v3

--- src/main/java/edu/scut/user_center/service/impl/UserServiceImpl.java
+++ src/main/java/edu/scut/user_center/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import edu.scut.user_center.model.entity.User;
 import edu.scut.user_center.service.UserService;
 import edu.scut.user_center.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,8 +20,13 @@ import java.util.List;
 * @createDate 2025-03-20 16:31:39
 */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
+
+    // 盐值，混淆密码
+    private static final String SALT = "YSQ";
+
     /**
      * 用户注册
      * @param userAccount 用户账号
@@ -39,7 +45,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         if (userAccount.length() < 4) {
             return -1;
         }
-        if (userPassword.length() < 8) {
+        if (userPassword.length() < 8 || confirmPassword.length() < 8) {
             return -1;
         }
 
@@ -63,7 +69,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         }
 
         // 密码加密
-        final String SALT = "YSQ"; // 盐值
         String securePassword = SecureUtil.md5(SALT + userPassword);
 
         // 将数据插入数据库
@@ -76,6 +81,49 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         }
 
         return user.getId();
+    }
+
+
+    /**
+     * 用户登录
+     * @param userAccount 用户账号
+     * @param userPassword 用户密码
+     * @return 用户信息（脱敏后）
+     */
+    @Override
+    public User userLogin(String userAccount, String userPassword) {
+        // 非空校验
+        if (CharSequenceUtil.hasBlank(userAccount, userPassword, confirmPassword)) {
+            return null;
+        }
+
+        // 长度校验
+        if (userAccount.length() < 4) {
+            return null;
+        }
+        if (userPassword.length() < 8) {
+            return null;
+        }
+
+        // 账号不能包含特殊字符
+        boolean isMatch = ReUtil.isMatch(".*(\\pP|\\pS|\\s+).*", userAccount);
+        if (isMatch) {
+            return null;
+        }
+
+        // 校验密码是否输入正确
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_account", userAccount);
+        // 密码加密
+        String securePassword = SecureUtil.md5(SALT + userPassword);
+        queryWrapper.eq("user_password", securePassword);
+        User user = this.getOne(queryWrapper);
+        // 用户不存在
+        if (user == null) {
+            log.info("user Login failed, userAccount cannot match userPassword");
+            return null;
+        }
+        return user;
     }
 }
 
